aab77cdd107d4fa0de84e9bc1ea1e08e95b222d6
MHammadYou <MHammadY2.0@gmail.com>
2022-04-24 23:34:14 +0500

    Added basic sky class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 
+
+
+class Sky
+{
+private:
+  static const int skySize = 10;
+  char m_Sky[skySize][skySize]{};
+
+public:
+  Sky()
+  {
+    for (auto & i : m_Sky)
+    {
+      for (char & j : i)
+      {
+        j = '.';
+      }
+    }
+  }
+
+  void logSky()
+  {
+    for (int i = 0; i < skySize; i++)
+    {
+      for (int j = 0; j < skySize; j++)
+      {
+        std::cout << m_Sky[i][j];
+      }
+      std::cout << std::endl;
+    }
+  }
+};
+
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+  Sky sky{};
+
+  sky.logSky();
 }
