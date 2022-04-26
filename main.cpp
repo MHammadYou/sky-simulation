f18b7816947f39b2b60a988243100c4a4a80adbd
MHammadYou <MHammadY2.0@gmail.com>
2022-04-26 23:33:04 +0500

    completed logSky method

--- main.cpp
+++ main.cpp
@@ -5,31 +5,44 @@
 class Sky
 {
 private:
-  static const int skySize = 10;
-  char m_Sky[skySize][skySize]{};
+  static const int skySize = 30;
+  std::string m_Sky[skySize][skySize]{};
 
 public:
   explicit Sky()
   {
     for (auto & i : m_Sky)
     {
-      for (char & j : i)
+      for (std::string & j : i)
       {
-        j = '.';
+        j = " . ";
       }
     }
   }
 
   void logSky()
   {
+    std::cout << " ";
+    for (int i = 0; i < skySize; i++)
+    {
+      std::cout << "___";
+    }
+    std::cout << std::endl;
     for (auto & i : m_Sky)
     {
-      for (char j : i)
+      std::cout << "|";
+      for (std::string j : i)
       {
         std::cout << j;
       }
-      std::cout << std::endl;
+      std::cout << "|" << std::endl;
     }
+    std::cout << " ";
+    for (int i = 0; i < skySize; i++)
+    {
+      std::cout << "___";
+    }
+    std::cout << std::endl;
   }
 
   void nextFrame()
